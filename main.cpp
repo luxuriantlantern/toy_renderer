8c2f24e5c2b676ad1d5f3f7e36795ecb39dc4525
luxuriantlantern <sjtu_chenlingxi@sjtu.edu.cn>
2025-03-18 23:39:08 +0800

    25_3_18_final

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 // Created by clx on 25-3-18.
 //
 
+#include <GLFW/glfw3.h>
 #include "ui/ui_manager.h"
 #include "camera/camera.h"
-#include <GLFW/glfw3.h>
 
 int main()
 {
@@ -23,6 +23,21 @@ int main()
     ui_manager.init();
 
     while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        float centerX = 800.0f / 2.0f;
+        float centerY = 600.0f / 2.0f;
+        float halfWidth = 200.0f;
+        float halfHeight = 150.0f;
+
+        glBegin(GL_QUADS);
+        glColor3f(1.0f, 1.0f, 1.0f);
+        glVertex2f((centerX - halfWidth) / 400.0f - 1.0f, (centerY - halfHeight) / 300.0f - 1.0f);
+        glVertex2f((centerX + halfWidth) / 400.0f - 1.0f, (centerY - halfHeight) / 300.0f - 1.0f);
+        glVertex2f((centerX + halfWidth) / 400.0f - 1.0f, (centerY + halfHeight) / 300.0f - 1.0f);
+        glVertex2f((centerX - halfWidth) / 400.0f - 1.0f, (centerY + halfHeight) / 300.0f - 1.0f);
+        glEnd();
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
