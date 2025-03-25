aa6a81e791ebfd1d136a4961fca4144c57480709
luxuriantlantern <sjtu_chenlingxi@sjtu.edu.cn>
2025-03-25 11:02:27 +0800

    TODO: Finish json loader

--- main.cpp
+++ main.cpp
@@ -8,12 +8,50 @@
 #include "io/loader.h"
 
 // Window dimensions
-const unsigned int SCR_WIDTH = 2560;
-const unsigned int SCR_HEIGHT = 2560;
+unsigned int SCR_WIDTH = 2560;
+unsigned int SCR_HEIGHT = 2560;
+CameraType CAM_TYPE = CameraType::PERSPECTIVE;
+
+void loadJSON(const std::string &path)
+{
+    std::ifstream ifs(path);
+    if (!ifs.is_open())
+    {
+        std::cerr << "Failed to open file: " << path << std::endl;
+        return;
+    }
+
+    std::stringstream buffer;
+    buffer << ifs.rdbuf();
+    std::string content = buffer.str();
+
+    size_t resPos = content.find("\"resolution\"");
+    size_t openBracket = content.find('[', resPos);
+    size_t comma = content.find(',', openBracket);
+    size_t closeBracket = content.find(']', comma);
+
+    SCR_WIDTH = std::stoi(content.substr(openBracket + 1, comma - openBracket - 1));
+    SCR_HEIGHT = std::stoi(content.substr(comma + 1, closeBracket - comma - 1));
+
+    size_t typePos = content.find("\"type\"", content.find("\"camera\""));
+    size_t typeValueStart = content.find(':', typePos) + 1;
+    size_t typeValueEnd = content.find(',', typeValueStart);
+    std::string cameraTypeStr = content.substr(typeValueStart, typeValueEnd - typeValueStart);
+
+    cameraTypeStr.erase(std::remove(cameraTypeStr.begin(), cameraTypeStr.end(), '\"'), cameraTypeStr.end());
+    cameraTypeStr.erase(std::remove(cameraTypeStr.begin(), cameraTypeStr.end(), ' '), cameraTypeStr.end());
+    cameraTypeStr.erase(std::remove(cameraTypeStr.begin(), cameraTypeStr.end(), '}'), cameraTypeStr.end());
+    cameraTypeStr.erase(std::remove(cameraTypeStr.begin(), cameraTypeStr.end(), '\n'), cameraTypeStr.end());
+
+
+    CAM_TYPE = (cameraTypeStr == "perspective") ?
+                            CameraType::PERSPECTIVE : CameraType::ORTHOGRAPHIC;
+}
 
 int main()
 {
-    auto mviewer = std::make_shared<Viewer>(SCR_WIDTH, SCR_HEIGHT, CameraType::PERSPECTIVE, Shadertype::PHONG, "Cube Renderer");
+    loadJSON("../assets/config.json");
+    auto mviewer = std::make_shared<Viewer>(SCR_WIDTH, SCR_HEIGHT, CAM_TYPE, Shadertype::PHONG, "Cube Renderer");
     auto mscene = mviewer->getScene();
     auto mloader = std::make_shared<Loader>();
     const std::string path = "../assets/cube.obj";
